b2beec1ba11dd796f920ee4ed8be6f0851fd1448
DESKTOP-U600PV3\User <woravootbat@gmail.com>
2024-04-30 03:50:04 +0700

    ✨ update school content

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -29,20 +29,20 @@ export default function Home() {
           sizes="33vw"
           src="/profile.jpg"
         />
-        <h1 class="mt-2 mb-8 text-2xl font-medium tracking-tighter">Hi, I am Worawut 👋</h1>
+        <h1 className="mt-2 mb-8 text-2xl font-medium tracking-tighter">Hi, I am Worawut 👋</h1>
       </div>
-      <p class="prose prose-neutral dark:prose-invert">
+      <p className="prose prose-neutral dark:prose-invert">
         I am Frontend Software Engineer. I currently
         <a href="/work"> work</a> as the Front end software engineer of Product at{' '}
-        <span class="not-prose">
+        <span className="not-prose">
           <a
             href="https://fictionlog.co/"
             target="_blank"
-            class="inline-flex items-center rounded border border-neutral-200 bg-neutral-50 p-1 text-sm leading-4 text-neutral-900 no-underline dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100"
+            className="inline-flex items-center rounded border border-neutral-200 bg-neutral-50 p-1 text-sm leading-4 text-neutral-900 no-underline dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100"
           >
             <Image
               className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert mx-1"
-              src="/fictionlog-logo.webp"
+              src="/logo/company/fictionlog-logo.webp"
               alt="Fictionlog Logo"
               width={14}
               height={14}
@@ -52,11 +52,11 @@ export default function Home() {
           </a>
         </span>{' '}
         and
-        <span class="not-prose">
+        <span className="not-prose">
           <a
             href="https://pintobook.com/"
             target="_blank"
-            class="inline-flex items-center rounded border border-neutral-200 bg-neutral-50 p-1 text-sm leading-4 text-neutral-900 no-underline dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100"
+            className="inline-flex items-center rounded border border-neutral-200 bg-neutral-50 p-1 text-sm leading-4 text-neutral-900 no-underline dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100"
           >
             <Image
               className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert mx-1"
@@ -73,9 +73,9 @@ export default function Home() {
         <a
           href="https://nextjs.org"
           target="_blank"
-          class="inline-flex items-center rounded border border-neutral-200 bg-neutral-50 p-1 text-sm leading-4 text-neutral-900 no-underline dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100"
+          className="inline-flex items-center rounded border border-neutral-200 bg-neutral-50 p-1 text-sm leading-4 text-neutral-900 no-underline dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100"
         >
-          <Image alt="Next.js logomark" src="logo/tech/next-logo.svg" class="!mr-1" width="14" height="14" />
+          <Image alt="Next.js logomark" src="logo/tech/next-logo.svg" className="!mr-1" width="14" height="14" />
           Nextjs
         </a>{' '}
         .
@@ -96,7 +96,7 @@ export default function Home() {
                   height="64"
                   className="h-16 w-16 rounded-full border border-neutral-200 dark:border-neutral-700"
                   sizes="33vw"
-                  src="/github_logo.png"
+                  src="/logo/tech/github_logo.png"
                 />
                 <div className="relative -right-10 -top-6 inline-flex h-8 w-8 items-center rounded-full border border-neutral-200 bg-white p-1 dark:border-neutral-700">
                   <Image
@@ -139,7 +139,7 @@ export default function Home() {
                   height="64"
                   className="h-16 w-16 rounded-full border border-neutral-200 dark:border-neutral-700"
                   sizes="33vw"
-                  src="/linkedin_logo.png"
+                  src="/logo/social/linkedin_logo.png"
                 />
                 <div className="relative -right-10 -top-6 inline-flex h-8 w-8 items-center rounded-full border border-neutral-200 bg-white p-1 dark:border-neutral-700">
                   <Image
@@ -189,18 +189,53 @@ export default function Home() {
         </div>
       </section>
 
-      <section>
+      <section className="space-y-4">
         <h3 className="font-medium text-lg">Education:</h3>
-        <p>
-          <span className="font-semibold">Kasetsart University</span>
-          <span className="text-neutral-600 dark:text-neutral-400 text-sm"> Bachelor of Science, Computer Science (2014 - 2018)</span>
-          <p>G.P.A. 3.27</p>
-        </p>
-        <p>
-          <span className="font-semibold">Chonradsadornumrung School</span>
-          <span className="text-neutral-600 dark:text-neutral-400 text-sm"> High School , Scinece-Mathematic (2010 - 2013)</span>
-          <p>G.P.A. 3.49</p>
-        </p>
+        <div className="group flex w-full">
+          <div className="flex w-full items-center justify-between rounded border border-neutral-200 bg-neutral-50 px-3 py-4 dark:border-neutral-700 dark:bg-neutral-800">
+            <div className="flex items-center space-x-3">
+              <div className="relative h-16">
+                <Image
+                  alt="@worawutbat"
+                  fetchpriority="high"
+                  width="64"
+                  height="64"
+                  className="h-16 w-16 rounded-full border border-neutral-200 dark:border-neutral-700"
+                  sizes="33vw"
+                  src="/logo/school/KU.png"
+                />
+              </div>
+              <div className="flex flex-col">
+                <p className="font-semibold text-neutral-900 dark:text-neutral-100">Kasetsart University</p>
+                <p className="text-neutral-600 dark:text-neutral-400 text-sm">Bachelor of Science, Computer Science (2014 - 2018)</p>
+                <p>G.P.A. 3.27</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="group flex w-full">
+          <div className="flex w-full items-center justify-between rounded border border-neutral-200 bg-neutral-50 px-3 py-4 dark:border-neutral-700 dark:bg-neutral-800">
+            <div className="flex items-center space-x-3">
+              <div className="relative h-16">
+                <Image
+                  alt="@worawutbat"
+                  fetchpriority="high"
+                  width="64"
+                  height="64"
+                  className="h-16 w-16 rounded-full border border-neutral-200 dark:border-neutral-700"
+                  sizes="33vw"
+                  src="/logo/school/cru.png"
+                />
+              </div>
+              <div className="flex flex-col">
+                <p className="font-semibold text-neutral-900 dark:text-neutral-100">Chonradsadornumrung School</p>
+                <p className="text-neutral-600 dark:text-neutral-400 text-sm">High School , Scinece-Mathematic (2010 - 2013)</p>
+                <p>G.P.A. 3.49</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </section>
   );
